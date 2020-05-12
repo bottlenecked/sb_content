35c37c9a64adab3761d53d4fe0396de91dcc28d6
Manos Emmanouilidis <m.emmanouilidis@stoiximan.gr>
2020-05-12 10:27:56 +0300

    chore(graphql schema): move type definitions outside schema.ex

--- sb_graphql/lib/schema.ex
+++ sb_graphql/lib/schema.ex
@@ -1,28 +1,9 @@
 defmodule SbGraphql.Schema do
   use Absinthe.Schema
-  alias SbGraphql.Resolvers.{EventResolvers}
 
-  input_object :event_filter do
-    field(:sport_id, list_of(:id))
-    field(:zone_id, list_of(:id))
-    field(:league_id, list_of(:id))
-    field(:active, :boolean)
-    field(:displayed, :boolean)
-    field(:live, :boolean)
-  end
+  import_types(__MODULE__.EventTypes)
 
   query do
-    field :events, list_of(:event) do
-      arg(:operator_id, non_null(:id))
-      arg(:filters, :event_filter)
-
-      resolve(&EventResolvers.events/3)
-    end
-  end
-
-  object :event do
-    field(:id, :id)
-    field(:sport_id, :id)
-    # field(:live?, :boolean)
+    import_fields(:event_queries)
   end
 end
---
+++ sb_graphql/lib/schema/event_types.ex
@@ -0,0 +1,30 @@
+defmodule SbGraphql.Schema.EventTypes do
+  use Absinthe.Schema.Notation
+  alias SbGraphql.Resolvers.EventResolvers
+
+  input_object :event_filter do
+    field(:sport_id, list_of(:id))
+    field(:zone_id, list_of(:id))
+    field(:league_id, list_of(:id))
+    field(:active, :boolean)
+    field(:displayed, :boolean)
+    field(:live, :boolean)
+  end
+
+  object :event do
+    field(:id, :id)
+    field(:sport_id, :id)
+    field(:type_id, :id)
+    field(:league_id, :id)
+    # field(:live?, :boolean)
+  end
+
+  object :event_queries do
+    field :events, list_of(:event) do
+      arg(:operator_id, non_null(:id))
+      arg(:filters, :event_filter)
+
+      resolve(&EventResolvers.events/3)
+    end
+  end
+end
